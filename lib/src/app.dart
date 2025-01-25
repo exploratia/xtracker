71ca8b1677aba81244a98ce4e64c2ae53251aebe
Christian Adler <chris.adler@gmx.de>
2025-01-25 14:05:57 +0100

    don't use material3

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -59,7 +59,11 @@ class MyApp extends StatelessWidget {
           // Define a light and dark color theme. Then, read the user's
           // preferred ThemeMode (light, dark, or system default) from the
           // SettingsController to display the correct theme.
-          theme: ThemeData(),
+          theme: ThemeData(
+            useMaterial3: false,
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.blueAccent), // TODO theme data
+          ),
           darkTheme: ThemeData.dark(),
           themeMode: settingsController.themeMode,
           locale: settingsController.locale,
