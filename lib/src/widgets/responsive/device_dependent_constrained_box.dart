147cc9b6b19581292b061b14947ed4ce5e67c05c
Christian Adler <chris.adler@gmx.de>
2025-01-28 05:09:12 +0100

    add device dependent constrained box widget

---
+++ lib/src/widgets/responsive/device_dependent_constrained_box.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../util/media_query_utils.dart';
+
+class DeviceDependentWidthConstrainedBox extends StatelessWidget {
+  static const double tabletMaxWidth = 700;
+  static const double tabletMinWidth = 500;
+
+  const DeviceDependentWidthConstrainedBox({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryInfo = MediaQueryUtils(MediaQuery.of(context));
+
+    if (mediaQueryInfo.isTablet) {
+      return ConstrainedBox(
+        // return Container(        color: Colors.red,
+        constraints: const BoxConstraints(
+          minWidth: tabletMinWidth,
+          maxWidth: tabletMaxWidth,
+        ),
+        child: child,
+      );
+    }
+
+    return child;
+  }
+}
