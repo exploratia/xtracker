4826c3707aef5bc045546095a88533ce5d6c85ef
Christian Adler <chris.adler@gmx.de>
2025-02-05 21:38:38 +0100

    improve OverflowText (multiline)

--- lib/src/widgets/text/overflow_text.dart
+++ lib/src/widgets/text/overflow_text.dart
@@ -2,23 +2,26 @@ import 'package:flutter/material.dart';
 
 class OverflowText extends StatelessWidget {
   /// Widget for TextOverflow - ellipses or fade
-  const OverflowText(this.text, {super.key, this.textOverflow = TextOverflow.ellipsis, this.style});
+  const OverflowText(this.text,
+      {super.key,
+      this.maxLines = 1,
+      this.textOverflow = TextOverflow.ellipsis,
+      this.style});
 
   final String text;
   final TextOverflow? textOverflow;
   final TextStyle? style;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: SizedBox(
-        child: Text(
-          text,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 1,
-          softWrap: false,
-          style: style,
-        ),
+    return Expanded(
+      child: Text(
+        text,
+        overflow: textOverflow,
+        maxLines: maxLines,
+        softWrap: false,
+        style: style,
       ),
     );
   }
