dbd9e4837352ac1ca897c20c7195a61e69d313a9
Christian Adler <chris.adler@gmx.de>
2025-02-05 21:03:11 +0100

    add navigate back icon button

---
+++ lib/src/widgets/navigation/nav_back_icon_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NavBackIconButton extends StatelessWidget {
+  const NavBackIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () => Navigator.pop(context),
+    );
+  }
+}
