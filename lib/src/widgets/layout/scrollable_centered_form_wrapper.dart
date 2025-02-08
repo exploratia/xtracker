1bc0b210c54c64adc8debfcfcd6a0ce15fe61fa2
Christian Adler <chris.adler@gmx.de>
2025-02-08 18:59:39 +0100

    add scrollable centered form wrapper

---
+++ lib/src/widgets/layout/scrollable_centered_form_wrapper.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../responsive/device_dependent_constrained_box.dart';
+import 'center_horizontal.dart';
+import 'single_child_scroll_view_with_scrollbar.dart';
+
+/// Wrapper fuer Formulare.<br>
+/// Scrollbar, zentriert, Padding und formKey
+class ScrollableCenteredFormWrapper extends StatelessWidget {
+  final Key formKey;
+  final List<Widget> children;
+
+  const ScrollableCenteredFormWrapper(
+      {super.key, required this.formKey, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollViewWithScrollbar(
+      child: CenterH(
+        child: DeviceDependentWidthConstrainedBox(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: children,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
