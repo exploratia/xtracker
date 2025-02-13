a605d3965409d25f5a2c3488bf857a048e0bc80f
Christian Adler <chris.adler@gmx.de>
2025-02-13 09:21:49 +0100

    restrict series items width

--- lib/src/widgets/series/series_management_view.dart
+++ lib/src/widgets/series/series_management_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/series_provider.dart';
+import '../responsive/device_dependent_constrained_box.dart';
 import 'series_def_renderer.dart';
 
 class SeriesManagementView extends StatelessWidget {
@@ -10,7 +11,6 @@ class SeriesManagementView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var series = context.watch<SeriesProvider>().series;
     final themeData = Theme.of(context);
     final t = AppLocalizations.of(context)!;
 
@@ -22,18 +22,28 @@ class SeriesManagementView extends StatelessWidget {
           IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.edit_off_outlined)),
         ],
       ),
-      body: ReorderableListView(
-        buildDefaultDragHandles: true,
-        padding: const EdgeInsets.all(16),
-        children: [
-          ...series.map((s) => SeriesDefRenderer(
-                key: Key(s.uuid),
-                editMode: true,
-                seriesDef: s,
-              ))
-        ],
-        onReorder: (int oldIndex, int newIndex) => context.read<SeriesProvider>().reorder(oldIndex, newIndex),
-      ),
+      body: const Center(child: DeviceDependentWidthConstrainedBox(child: _SeriesList())),
+    );
+  }
+}
+
+class _SeriesList extends StatelessWidget {
+  const _SeriesList();
+
+  @override
+  Widget build(BuildContext context) {
+    var series = context.watch<SeriesProvider>().series;
+    return ReorderableListView(
+      buildDefaultDragHandles: true,
+      padding: const EdgeInsets.all(16),
+      children: [
+        ...series.map((s) => SeriesDefRenderer(
+              key: Key(s.uuid),
+              editMode: true,
+              seriesDef: s,
+            )),
+      ],
+      onReorder: (int oldIndex, int newIndex) => context.read<SeriesProvider>().reorder(oldIndex, newIndex),
     );
   }
 }
