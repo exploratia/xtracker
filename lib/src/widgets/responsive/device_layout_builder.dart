3b2d1aa6654c40566285a8e9391a019db89e8f0f
Christian Adler <chris.adler@gmx.de>
2025-02-13 08:57:36 +0100

    add device layout builder

---
+++ lib/src/widgets/responsive/device_layout_builder.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../../util/media_query_utils.dart';
+
+class DeviceLayoutBuilder extends StatelessWidget {
+  final Widget Function(BuildContext context)? tabletBuilder;
+  final Widget Function(BuildContext context) phoneBuilder;
+
+  const DeviceLayoutBuilder({super.key, this.tabletBuilder, required this.phoneBuilder});
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryInfo = MediaQueryUtils(MediaQuery.of(context));
+    if (mediaQueryInfo.isTablet && tabletBuilder != null) {
+      return tabletBuilder!(context);
+    }
+
+    return phoneBuilder(context);
+  }
+}
