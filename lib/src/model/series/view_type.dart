210bde1de3e34298f703553671547139f43e6782
Christian Adler <chris.adler@gmx.de>
2025-02-26 04:50:44 +0100

    adjust view_type text and icons

--- lib/src/model/series/view_type.dart
+++ lib/src/model/series/view_type.dart
@@ -3,8 +3,13 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 enum ViewType {
   chart(Icons.area_chart_outlined),
-  table(Icons.table_chart_outlined),
-  dots(Icons.calendar_month_outlined);
+  table(Icons.grid_on_outlined),
+  // table(Icons.table_chart_outlined),
+  dots(Icons.margin_outlined);
+  // dots(Icons.drag_indicator_outlined);
+  // dots(Icons.dataset_outlined);
+  // dots(Icons.blur_linear_outlined);
+  // dots(Icons.apps_rounded);
 
   final IconData iconData;
 
@@ -12,9 +17,9 @@ enum ViewType {
 
   static String displayNameOf(ViewType seriesType, AppLocalizations t) {
     return switch (seriesType) {
-      ViewType.chart => 'Chart',
-      ViewType.table => 'Table',
-      ViewType.dots => 'Dots',
+      ViewType.chart => t.seriesViewTypeChart,
+      ViewType.table => t.seriesViewTypeTable,
+      ViewType.dots => t.seriesViewTypeDots,
     };
   }
 }
