4c05b42c5f4ad64b95becc66643c4aa5f291d147
Christian Adler <chris.adler@gmx.de>
2025-05-24 20:38:51 +0200

    set keyboard type for blood pressure

--- lib/src/widgets/series/data/input/blood_pressure/blood_pressure_input.dart
+++ lib/src/widgets/series/data/input/blood_pressure/blood_pressure_input.dart
@@ -198,6 +198,7 @@ class _BloodPressureQuickInputState extends State<BloodPressureQuickInput> {
                       ),
                       // Only numbers can be entered:
                       inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
+                      keyboardType: const TextInputType.numberWithOptions(signed: false, decimal: false),
                       textInputAction: TextInputAction.done,
                       // unicode is possible - e.g. from https://www.compart.com/de/unicode/block/U+1F600
                       validator: (value) {
