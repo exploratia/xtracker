30f63336cf83f6338b54996daf3ddd8f8cc2c3df
Christian Adler <chris.adler@gmx.de>
2025-06-01 05:33:54 +0200

    set series management button always active

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,12 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../../generated/assets.gen.dart';
 import '../../generated/locale_keys.g.dart';
 import '../model/navigation/main_navigation_item.dart';
 import '../model/series/series_def.dart';
-import '../providers/series_provider.dart';
 import '../widgets/layout/gradient_app_bar.dart';
 import '../widgets/navigation/hide_bottom_navigation_bar.dart';
 import '../widgets/responsive/screen_builder.dart';
@@ -22,28 +20,20 @@ class HomeScreen extends StatelessWidget {
 
   const HomeScreen({super.key});
 
+  void _showSeriesManagement(BuildContext context) async {
+    final themeData = Theme.of(context);
+    await showDialog<SeriesDef>(
+        context: context,
+        builder: (context) {
+          return Dialog.fullscreen(
+            backgroundColor: themeData.scaffoldBackgroundColor,
+            child: const HideBottomNavigationBar(child: SeriesManagementView()),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final themeData = Theme.of(context);
-
-    // Reorder possible?
-    Future Function()? showReorderDlg;
-
-    if (context.watch<SeriesProvider>().series.isNotEmpty) {
-      showReorderDlg = () async {
-        await showDialog<SeriesDef>(
-            context: context,
-            builder: (context) {
-              return Dialog.fullscreen(
-                backgroundColor: themeData.scaffoldBackgroundColor,
-                child: const HideBottomNavigationBar(child: SeriesManagementView()),
-              );
-            });
-      };
-    } else {
-      showReorderDlg = null;
-    }
-
     return ScreenBuilder.withStandardNavBuilders(
       navItem: navItem,
       appBarBuilder: (context) {
@@ -62,7 +52,7 @@ class HomeScreen extends StatelessWidget {
                     /*SeriesDef? s=*/ await SeriesDef.addNewSeries(context);
                   },
                   icon: const Icon(Icons.add_chart_outlined)),
-              IconButton(onPressed: showReorderDlg, icon: const Icon(Icons.edit_outlined)),
+              IconButton(onPressed: () => _showSeriesManagement(context), icon: const Icon(Icons.edit_outlined)),
             ]);
       },
       bodyBuilder: (context) => const SeriesView(),
