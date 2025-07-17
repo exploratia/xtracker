20d7940c9a14ea0f2ba5acba7180ad6ad5c1085b
Christian Adler <chris.adler@gmx.de>
2025-07-17 21:43:21 +0200

    fix #6: correct file and line information in logs
    example:
    [2025-07-17 21:42:32.122] WARN  | log_settings_view.dart:57 | Warning Message
    [2025-07-17 21:42:32.131] ERROR | log_settings_view.dart:58 | Error Message

--- lib/src/util/logging/flutter_simple_logging.dart
+++ lib/src/util/logging/flutter_simple_logging.dart
@@ -179,7 +179,7 @@ class _Outputs {
       logMsg += line;
     }
 
-    logMsg = '${_normalizedLevel(event.level)} | ${fileLineAndStack.fileLine} $logMsg';
+    logMsg = '${_normalizedLevel(event.level)} | ${fileLineAndStack.toLogFileLine()} | $logMsg';
     if (error != null) {
       logMsg += '\n  Error:\n$error';
     }
@@ -197,7 +197,7 @@ class _Outputs {
       if (kDebugMode) {
         if (first) {
           first = false;
-          print('${_normalizedLevel(event.level)} ${fileLineAndStack.fileLine} $line');
+          print('${_normalizedLevel(event.level)} ${fileLineAndStack.toConsoleFileLine()} | $line');
         } else {
           print(line);
         }
@@ -225,40 +225,71 @@ class _Outputs {
 
 class _StackUtils {
   static final packageName = AppInfo.projectName;
+  static final regexBetweenBrackets = RegExp(r'\((.*?)\)');
+  static final regexFile = RegExp(r'(package:(.+\.dart))');
+  static final regexLineColumn = RegExp(r'(\d+):(\d+)');
 
   static _FileLineAndStack determineFileLine() {
     // print(StackTrace.current);
-    final stack = StackTrace.current
-        .toString()
-        .replaceAll('<anonymous closure>', '')
-        .replaceAll('<asynchronous suspension>', '')
-        .split('\n')
-        .where((line) => !line.contains('package:logger') && !line.contains('package:flutter_simple_logging/flutter_simple_logging') && line.trim().isNotEmpty)
+    var currentStackTrace = StackTrace.current.toString();
+    var currentStackTraceLines = currentStackTrace.replaceAll('<anonymous closure>', '').replaceAll('<asynchronous suspension>', '').split('\n');
+    var filteredStackTraceLines = currentStackTraceLines
+        .where((line) => !line.contains('package:logger') && !line.contains('flutter_simple_logging.dart') && line.trim().isNotEmpty)
         .where((line) {
-          // FullStack? Dann immer True
-          if (SimpleLogging.useFullStack) return true;
-          // Sonst nur packageName erlauben
-          return line.contains('package:$packageName/') || line.contains('package:flutter_simple_logging/widgets/logs_view');
-        }) // ist das gut? wg. Fremdpaketen evtl.?
-        .map((line) => line.substring(line.indexOf(' ')).trimLeft())
-        .toList();
+      // FullStack? Dann immer True
+      if (SimpleLogging.useFullStack) return true;
+      // Sonst nur packageName erlauben
+      return line.contains('package:$packageName/') || line.contains('package:flutter_simple_logging/widgets/logs_view');
+    }); // ist das gut? wg. Fremdpaketen evtl.?
 
-    if (stack.isEmpty) return _FileLineAndStack('-?-', null);
-
-    String fileLine = stack.first;
-    // fileLine = fileLine.substring(fileLine.indexOf('('));
-    // man koennte noch den standard-Package-Pfad kuerzen, aber dann kann man in der Console beim Debug nicht mehr klicken :(
-    //.replaceFirst('package:$packageName', '..');
+    final stack = filteredStackTraceLines.toList(); // .map((line) => line.substring(0, line.indexOf(' ')).trim()).toList();
 
     final String? s = stack.length > 1 ? stack.join('\n') : null;
 
-    return _FileLineAndStack(fileLine, s);
+    String fileName = '-?-';
+    String fileFullName = '-?-';
+    String lineNo = '-?-';
+
+    if (stack.isNotEmpty) {
+      String firstStackLine = stack.first;
+      final matchBetweenBrackets = regexBetweenBrackets.firstMatch(firstStackLine);
+      if (matchBetweenBrackets != null) {
+        firstStackLine = matchBetweenBrackets.group(1)!;
+      }
+      final matchFile = regexFile.firstMatch(firstStackLine);
+      if (matchFile != null) {
+        String? str = matchFile.group(1); // Path to file
+        if (str != null) {
+          fileFullName = str;
+          fileName = str.split('/').last;
+        }
+      }
+      final matchLine = regexLineColumn.firstMatch(firstStackLine);
+      if (matchLine != null) {
+        String? str = matchLine.group(1);
+        if (str != null) {
+          lineNo = str;
+        }
+      }
+    }
+
+    return _FileLineAndStack(fileName, fileFullName, lineNo, s);
   }
 }
 
 class _FileLineAndStack {
-  final String fileLine;
+  final String fileName;
+  final String fileFullName;
+  final String lineNo;
   final String? stack;
 
-  _FileLineAndStack(this.fileLine, this.stack);
+  _FileLineAndStack(this.fileName, this.fileFullName, this.lineNo, this.stack);
+
+  String toConsoleFileLine() {
+    return '$fileFullName:$lineNo';
+  }
+
+  String toLogFileLine() {
+    return '$fileName:$lineNo';
+  }
 }
