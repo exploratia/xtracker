8b4c27b6504d32479ff4be35cd364e103a21232f
Christian Adler <chris.adler@gmx.de>
2025-09-09 20:18:09 +0200

    implement simple glowing progress bar

---
+++ lib/src/widgets/controls/progress/progress_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../../../util/chart/chart_utils.dart';
+import '../../../util/color_utils.dart';
+import '../../../util/theme_utils.dart';
+import '../card/glowing_border_container.dart';
+
+class ProgressBar extends StatelessWidget {
+  /// - [value] percentage value between 0 and 1
+  const ProgressBar({super.key, required this.value, this.padding, this.color});
+
+  final double value;
+  final EdgeInsetsGeometry? padding;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    Color col = color ?? ThemeUtils.secondary;
+    Color gradientColor = ColorUtils.hue(col, -30);
+    Widget progress = Container(
+      height: ThemeUtils.borderRadiusSmall * 2,
+      clipBehavior: Clip.antiAlias,
+      decoration: GlowingBorderContainer.createGlowingBoxDecoration(
+        col,
+        themeData.cardTheme.color,
+        borderRadius: ThemeUtils.borderRadiusSmall,
+        blurRadius: ThemeUtils.borderRadiusSmall,
+        borderWidth: 1,
+      ),
+      child: FractionallySizedBox(
+        alignment: AlignmentGeometry.topLeft,
+        widthFactor: value,
+        heightFactor: 1,
+        child: Container(
+          decoration: BoxDecoration(
+            // color: color ?? ThemeUtils.secondary,
+            gradient: ChartUtils.createLeftToRightGradient([gradientColor, col]),
+            borderRadius: BorderRadius.circular(ThemeUtils.borderRadiusSmall),
+          ),
+        ),
+      ),
+    );
+
+    if (padding != null) {
+      progress = Padding(
+        padding: padding!,
+        child: progress,
+      );
+    }
+
+    return progress;
+  }
+}
