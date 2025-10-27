dfc68d3b10734fa6ce7573969e8ba0a1f82f46ee
Christian Adler <chris.adler@gmx.de>
2025-10-27 04:50:49 +0100

    extract common logic for simple inputs in simple_dialog_input

--- lib/src/widgets/series/data/input/daily_life/daily_life_input.dart
+++ lib/src/widgets/series/data/input/daily_life/daily_life_input.dart
@@ -6,14 +6,11 @@ import '../../../../../../generated/locale_keys.g.dart';
 import '../../../../../model/series/data/daily_life/daily_life_value.dart';
 import '../../../../../model/series/series_def.dart';
 import '../../../../../model/series/settings/daily_life/daily_life_attribute_resolver.dart';
-import '../../../../../util/dialogs.dart';
 import '../../../../../util/media_query_utils.dart';
 import '../../../../../util/theme_utils.dart';
-import '../../../../controls/layout/single_child_scroll_view_with_scrollbar.dart';
-import '../../../../controls/text/overflow_text.dart';
 import '../../view/daily_life/daily_life_attribute_renderer.dart';
-import '../input_header.dart';
 import '../input_result.dart';
+import '../simple_dialog_input.dart';
 
 class DailyLifeInput extends StatefulWidget {
   const DailyLifeInput({super.key, this.dailyLifeValue, required this.seriesDef});
@@ -81,96 +78,52 @@ class _DailyLifeInputState extends State<DailyLifeInput> {
     Navigator.pop(context, InputResult(val, insert ? InputResultAction.insert : InputResultAction.update));
   }
 
-  void _deleteHandler(DailyLifeValue dailyLifeValue) async {
-    bool? res = await Dialogs.simpleYesNoDialog(
-      LocaleKeys.seriesValue_query_deleteValue.tr(),
-      context,
-      title: LocaleKeys.commons_dialog_title_areYouSure.tr(),
-    );
-    if (res == true && mounted) {
-      Navigator.pop(context, InputResult(dailyLifeValue, InputResultAction.delete));
+  void _deleteHandler() {
+    if (widget.dailyLifeValue != null && mounted) {
+      Navigator.pop(context, InputResult(widget.dailyLifeValue!, InputResultAction.delete));
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final themeData = Theme.of(context);
-    var iconSize = ThemeUtils.iconSizeScaled;
-
-    var edit = Column(
-      mainAxisSize: MainAxisSize.min,
-      spacing: ThemeUtils.verticalSpacing,
-      children: [
-        InputHeader(dateTime: _dateTime, setDateTime: _setDateTime),
-        const Divider(height: 1),
-        ConstrainedBox(
-          constraints: BoxConstraints(minHeight: 40 * MediaQueryUtils.textScaleFactor),
-          child: Center(
-            child: PopupMenuButton(
-              borderRadius: ThemeUtils.borderRadiusCircular,
-              icon: _attributeUuid == null ? const Icon(Icons.list) : null,
-              tooltip: LocaleKeys.seriesValue_dailyLife_btn_selectAttribute_tooltip.tr(),
-              itemBuilder: (context) => widget.seriesDef
-                  .dailyLifeAttributesSettingsReadonly()
-                  .attributes
-                  .map(
-                    (e) => PopupMenuItem(
-                      /* Flutter Bug? if right >= 10 a huge wider padding is used. Seems not to work always. If only Text-Widgets are uses as child it has no effect. */
-                      padding: const EdgeInsets.only(right: 9, left: 9, bottom: 0),
-                      onTap: () => _setAttributeUuid(e.aid),
-                      child: DailyLifeAttributeRenderer(dailyLifeAttribute: e),
-                    ),
-                  )
-                  .toList(),
-              child: _attributeUuid != null
-                  ? Padding(
-                      padding: const EdgeInsets.all(ThemeUtils.paddingSmall),
-                      child: DailyLifeAttributeRenderer(dailyLifeAttribute: _dailyLifeAttributeResolver.resolve(_attributeUuid)),
-                    )
-                  : null,
-            ),
-          ),
+    var edit = ConstrainedBox(
+      constraints: BoxConstraints(minHeight: 40 * MediaQueryUtils.textScaleFactor),
+      child: Center(
+        child: PopupMenuButton(
+          borderRadius: ThemeUtils.borderRadiusCircular,
+          icon: _attributeUuid == null ? const Icon(Icons.list) : null,
+          tooltip: LocaleKeys.seriesValue_dailyLife_btn_selectAttribute_tooltip.tr(),
+          itemBuilder: (context) => widget.seriesDef
+              .dailyLifeAttributesSettingsReadonly()
+              .attributes
+              .map(
+                (e) => PopupMenuItem(
+                  /* Flutter Bug? if right >= 10 a huge wider padding is used. Seems not to work always. If only Text-Widgets are uses as child it has no effect. */
+                  padding: const EdgeInsets.only(right: 9, left: 9, bottom: 0),
+                  onTap: () => _setAttributeUuid(e.aid),
+                  child: DailyLifeAttributeRenderer(dailyLifeAttribute: e),
+                ),
+              )
+              .toList(),
+          child: _attributeUuid != null
+              ? Padding(
+                  padding: const EdgeInsets.all(ThemeUtils.paddingSmall),
+                  child: DailyLifeAttributeRenderer(dailyLifeAttribute: _dailyLifeAttributeResolver.resolve(_attributeUuid)),
+                )
+              : null,
         ),
-      ],
+      ),
     );
 
-    return AlertDialog(
-      title: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: ThemeUtils.seriesDataInputDlgMaxWidth),
-        child: Row(
-          spacing: ThemeUtils.horizontalSpacing,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            widget.dailyLifeValue == null ? Icon(Icons.add_outlined, size: iconSize) : Icon(Icons.edit_outlined, size: iconSize),
-            OverflowText(widget.seriesDef.name),
-            if (widget.dailyLifeValue != null)
-              IconButton(
-                tooltip: LocaleKeys.seriesValue_action_deleteValue_tooltip.tr(),
-                onPressed: () => _deleteHandler(widget.dailyLifeValue!),
-                color: themeData.colorScheme.secondary,
-                iconSize: iconSize,
-                icon: const Icon(Icons.delete_outlined),
-              ),
-          ],
-        ),
-      ),
-      content: SingleChildScrollViewWithScrollbar(
-        child: edit,
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context, null);
-          },
-          child: Text(LocaleKeys.commons_dialog_btn_cancel.tr()),
-        ),
-        if (_isValid)
-          TextButton(
-            onPressed: _isValid ? _saveHandler : null,
-            child: Text(LocaleKeys.commons_dialog_btn_okay.tr()),
-          ),
-      ],
+    return SimpleDialogInput(
+      isEdit: widget.dailyLifeValue != null,
+      isValid: _isValid,
+      seriesDef: widget.seriesDef,
+      dateTime: _dateTime,
+      setDateTime: _setDateTime,
+      saveHandler: _saveHandler,
+      deleteHandler: _deleteHandler,
+      child: edit,
     );
   }
 }
---
+++ lib/src/widgets/series/data/input/simple_dialog_input.dart
@@ -0,0 +1,110 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../generated/locale_keys.g.dart';
+import '../../../../model/series/series_def.dart';
+import '../../../../util/dialogs.dart';
+import '../../../../util/theme_utils.dart';
+import '../../../controls/layout/single_child_scroll_view_with_scrollbar.dart';
+import '../../../controls/text/overflow_text.dart';
+import 'input_header.dart';
+
+class SimpleDialogInput extends StatelessWidget {
+  const SimpleDialogInput({
+    super.key,
+    required this.saveHandler,
+    required this.child,
+    required this.dateTime,
+    required this.deleteHandler,
+    required this.setDateTime,
+    required this.seriesDef,
+    required this.isEdit,
+    required this.isValid,
+    this.monthly = false,
+  });
+
+  final bool isEdit;
+  final bool isValid;
+  final SeriesDef seriesDef;
+  final DateTime dateTime;
+  final Function(DateTime value) setDateTime;
+  final bool monthly;
+  final VoidCallback saveHandler;
+  final VoidCallback deleteHandler;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    var iconSize = ThemeUtils.iconSizeScaled;
+
+    void showDelDlg() async {
+      bool? res = await Dialogs.simpleYesNoDialog(
+        LocaleKeys.seriesValue_query_deleteValue.tr(),
+        context,
+        title: LocaleKeys.commons_dialog_title_areYouSure.tr(),
+      );
+      if (res == true) {
+        deleteHandler();
+      }
+    }
+
+    var edit = Column(
+      mainAxisSize: MainAxisSize.min,
+      spacing: ThemeUtils.verticalSpacing,
+      children: [
+        InputHeader(
+          dateTime: dateTime,
+          setDateTime: setDateTime,
+          monthly: monthly,
+        ),
+        const Divider(height: 1),
+        child,
+      ],
+    );
+
+    return AlertDialog(
+      title: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: ThemeUtils.seriesDataInputDlgMaxWidth),
+        child: Row(
+          spacing: ThemeUtils.horizontalSpacing,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            isEdit ? Icon(Icons.edit_outlined, size: iconSize) : Icon(Icons.add_outlined, size: iconSize),
+            OverflowText(seriesDef.name),
+            if (isEdit)
+              IconButton(
+                tooltip: LocaleKeys.seriesValue_action_deleteValue_tooltip.tr(),
+                onPressed: () => showDelDlg(),
+                color: themeData.colorScheme.secondary,
+                iconSize: iconSize,
+                icon: const Icon(Icons.delete_outlined),
+              ),
+          ],
+        ),
+      ),
+      content: SingleChildScrollViewWithScrollbar(
+        child: edit,
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context, null);
+          },
+          child: Text(LocaleKeys.commons_dialog_btn_cancel.tr()),
+        ),
+        if (isValid)
+          TextButton(
+            onPressed: saveHandler,
+            child: Text(LocaleKeys.commons_dialog_btn_okay.tr()),
+          ),
+        if (!isValid && isEdit)
+          TextButton(
+            onPressed: () => showDelDlg(),
+            child: Text(LocaleKeys.commons_dialog_btn_delete.tr()),
+          ),
+      ],
+    );
+  }
+}
