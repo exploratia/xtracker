5d321da79ac892b2440f77c4dfad88adcb4e40e8
Christian Adler <chris.adler@gmx.de>
2026-05-15 06:30:04 +0200

    update ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     namespace = "de.exploratia.xtracker"
     compileSdk = flutter.compileSdkVersion
 //    ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
