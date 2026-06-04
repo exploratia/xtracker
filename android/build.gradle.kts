80030b422362f65844574238d98b74efb2e8409d
Christian Adler <christian.adler@usu.com>
2026-06-04 12:59:50 +0200

    correct build dir for subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,9 +8,14 @@ allprojects {
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+val rootProjectDirPath = rootProject.projectDir.toPath().toAbsolutePath().normalize()
+
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val subprojectDirPath = project.projectDir.toPath().toAbsolutePath().normalize()
+    if (subprojectDirPath.startsWith(rootProjectDirPath)) {
+        val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+        project.layout.buildDirectory.value(newSubprojectBuildDir)
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
